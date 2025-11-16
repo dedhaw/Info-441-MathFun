ebeaab5d9ca3dd32e392d7790f4e58b58e3b5e47
kirbyvo <kirbtxd@gmail.com>
2025-11-16 14:42:34 -0800

    environment var usage?

--- app.js
+++ app.js
@@ -3,3 +3,52 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import sessions from 'express-session';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import models from './models.js';
+
+// vars
+
+
+
+// use .env variables
+const authConfig = {
+    auth: {
+        clientId: process.env.CLIENT_ID,
+        authority: `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
+        clientSecret: process.env.CLIENT_SECRET,
+        redirectUri: "/redirect",
+    },
+	system: {
+    	loggerOptions: {
+        	loggerCallback(loglevel, message, containsPii) {
+            	console.log(message);
+        	},
+        	piiLoggingEnabled: false,
+        	logLevel: 3,
+    	}
+	}
+};
+const oneDay = 1000 * 60 * 60 * 24
+
+// routing
+
+var app = express();
+
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) =>{
+    req.models = models
+    next()
+})
+
+export default app;
--- models.js
+++ models.js
@@ -1,4 +1,24 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
+let models = {}
 
-let models = {};
+mongoose.connect(process.env.MONGOOSE_URI)
+console.log("connected to mongodb")
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    high_score: mongoose.Schema.Types.ObjectId,
+})
+
+const scoresSchema = new mongoose.Schema({
+    username: String,
+    score: Number,
+    date: Date,
+})
+
+const mathProblemSchema = new mongoose.Schema({
+    problem: String,
+    answer: Number,
+})
