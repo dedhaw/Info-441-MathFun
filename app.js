4159bb622ba59a0befd680675e4f09c6cce61448
kirbyvo <kirbtxd@gmail.com>
2025-11-16 14:51:50 -0800

    routes

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ import sessions from 'express-session';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import activityRouter from './routes/controllers/activity.js';
+import gameRouter from './routes/controllers/game.js';
+import matchRouter from './routes/controllers/match.js';
+import usersRouter from './routes/controllers/users.js';
 import models from './models.js';
 
 import { fileURLToPath } from 'url';
@@ -62,6 +66,10 @@ app.use((req, res, next) =>{
 })
 // routing
 
+app.use('/activity', activityRouter);
+app.use('/game', gameRouter);
+app.use('/match', matchRouter);
+app.use('/users', usersRouter);
 
 // auth
 const authProvider = await WebAppAuthProvider.WebAppAuthProvider.initialize(authConfig);
