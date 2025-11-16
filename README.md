37f168e2b0c3f72b6c714a39b58a422d0089eeaf
kirbyvo <kirbtxd@gmail.com>
2025-11-16 14:56:58 -0800

    more user data

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Info-441-MathFun
 Info 441 Final Project
+
--- models.js
+++ models.js
@@ -1,37 +1,43 @@
-import mongoose from "mongoose";
-import dotenv from 'dotenv';
-dotenv.config();
+  import mongoose from "mongoose";
+  import dotenv from 'dotenv';
+  dotenv.config();
 
-let models = {}
+  let models = {}
 
-mongoose.connect(process.env.MONGOOSE_URI)
-console.log("connected to mongodb")
-
-
-// user
-const userSchema = new mongoose.Schema({
-    username: String,
-    high_score: mongoose.Schema.Types.ObjectId,
-})
-models.User = mongoose.model('User', userSchema)
-console.log("created user model")
-
-// scores
-const scoresSchema = new mongoose.Schema({
-    username: String,
-    score: Number,
-    date: Date,
-})
-models.Scores = mongoose.model('Scores', scoresSchema)
-console.log("created scores model")
+  mongoose.connect(process.env.MONGOOSE_URI)
+  console.log("connected to mongodb")
 
 
-// math problems
-const mathProblemSchema = new mongoose.Schema({
-    problem: String,
-    answer: Number,
-})
-models.MathProblem = mongoose.model('MathProblem', mathProblemSchema)
-console.log("created math problem model")
+  // user
+  const userSchema = new mongoose.Schema({
+      username: String,
+      friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+      friend_requests: [{
+          from: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+          date: Date
+      }],
+      high_score: mongoose.Schema.Types.ObjectId,
+      last_online: Date,
+  })
+  models.User = mongoose.model('User', userSchema)
+  console.log("created user model")
 
-export default models;
+  // scores
+  const scoresSchema = new mongoose.Schema({
+      username: String,
+      score: Number,
+      date: Date,
+  })
+  models.Scores = mongoose.model('Scores', scoresSchema)
+  console.log("created scores model")
+
+
+  // math problems
+  const mathProblemSchema = new mongoose.Schema({
+      problem: String,
+      answer: Number,
+  })
+  models.MathProblem = mongoose.model('MathProblem', mathProblemSchema)
+  console.log("created math problem model")
+
+  export default models;
